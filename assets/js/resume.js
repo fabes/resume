cb62176c650ef1aa0037cf3bb1310dd5886819ee
Fabion Stephens <f.o.stephens@gmail.com>
2015-06-06 02:43:54 -0500

    getting the basics of AnguarJS setup. So fa sweeet!

---
+++ assets/js/resume.js
@@ -0,0 +1,23 @@
+'use strict'; //Based on research, this simply means to execute the code in 'strict mode'
+
+/* Declare our module for AngularJS and ensure ngapp on the <html>
+ * is update with our var below for things to work properly
+ */
+var resumeApp = angular.module('resumeApp', []);
+
+//Declare a header controller with an array of objects
+resumeApp.controller('headerCtrl', function($scope) {
+    $scope.biocontact =
+        {
+            img: 'assets/images/unexpected_head_shot.jpg',
+            name: 'Fabion O. Stephens',
+            address:
+                {
+                    street: '25 Timber Path',
+                    city: 'Kingston',
+                    country: 'Jamaica'
+                },
+            telephone: '1876-389-5264',
+            email: 'fabion@thestephenshq.com'
+        };
+});
